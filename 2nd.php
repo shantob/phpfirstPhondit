95bd781578c7866758c8d909bdd29a1154fd2c3c
Shanto Bepary <shantobepary575@gmail.com>
2022-08-19 23:50:28 +0600

    add a new file

--- 2nd.php
+++ 2nd.php
@@ -7,10 +7,58 @@
     <title>2nd Page</title>
 </head>
 <body>
+  
     <?php
+// $resource = " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores facere autem optio vitae magnam numquam necessitatibus magni natus sunt velit.";
+// $string = <<<FORM
 
-$x = 5;
-$y = 10;
+// <h1>This is nowadoc program $resource</h1>
+// FORM;
+// echo $string;
+
+
+
+
+
+
+// string handaling finction........................................////
+$name = " bangladesh is a beautiful Country  <br>  ";
+
+// echo strlen($name);
+// echo "<br>";
+// echo strtoupper($name);
+// echo "<br>";
+// echo strtolower($name);
+// echo "<br>";
+// echo ucfirst($name);
+// echo "<br>";
+// echo ucwords($name);
+// echo "<br>";
+
+// echo str_repeat($name ,3);
+
+// echo str_replace("Country" , "", $name);
+//echo str_replace(" " , "", $name);
+// remove start and end space........................................
+// echo "<br>";
+// echo strlen($name);
+// echo "<br>";
+// $tremSpace = trim($name);
+// echo $tremSpace;
+// echo strlen($tremSpace);
+
+// $arr = array('Name ',' Shanto',' Roll ','06');
+// //echo implode(" ",$arr); //same to join
+// $data= join(" ",$arr); //same to implode
+// echo $data;
+// echo "<br>";
+// print_r(explode(" ", $data));
+
+// echo nl2br("Bangladesh \n long Life");
+// echo number_format(100000,2);
+
+
+
 
 
 ?>
--- index.php
+++ index.php
@@ -1,55 +1,90 @@
-<!doctype html>
-<html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>First php</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-</head>
-
-<body class="container mt-5">
-    <div class="card">
-        <div class="card-body">
-
+   
             <?php
 
 
-            $cars = array("Volvo", "BMW", "Toyota");
-            var_dump($cars);
-            echo "<br>";
-            // ..............................
+            // $cars = array("Volvo", "BMW", "Toyota");
+            // var_dump($cars);
+            // echo "<br>";
+            // // ..............................
 
-            class Car
-            {
-                public $color;
-                public $model;
-                public function __construct($color, $model)
-                {
-                    $this->color = $color;
-                    $this->model = $model;
-                }
-                public function message()
-                {
-                    return "My car is a " . $this->color . " " . $this->model . "!";
-                }
-            }
+            // class Car
+            // {
+            //     public $color;
+            //     public $model;
+            //     public function __construct($color, $model)
+            //     {
+            //         $this->color = $color;
+            //         $this->model = $model;
+            //     }
+            //     public function message()
+            //     {
+            //         return "My car is a " . $this->color . " " . $this->model . "!";
+            //     }
+            // }
 
-            $myCar = new Car("black", "Volvo");
-            echo $myCar->message();
-            echo "<br>";
-            $myCar = new Car("red", "Toyota");
-            echo $myCar->message();
-            echo "<br>";
+            // $myCar = new Car("black", "Volvo");
+            // echo $myCar->message();
+            // echo "<br>";
+            // $myCar = new Car("red", "Toyota");
+            // echo $myCar->message();
+            // echo "<br>";
 
+            // require_once "2nd.php";
+            // $sum = "$x + $y";
+            // echo $sum;
+            // ..............................................
+            // function...........................................
+            // function number($number1 = 30, $number2 = 26)
+            // {
+            //     echo $number1 + $number2;
+            // }
+            // number(36, 40);
+            // number();
+            // ninus function................................./
+            // function sum($number1, $number2)
+            // {
+            //     echo $number1 + $number2;
+            // }
+            // function minus($number1, $number2)
+            // {
+            //     echo $number1 - $number2;
+            // }
+            // minus(26, 30);
+            // echo '</br>';
+            // sum(26, 30);
+
+            // if (!isset($math)) {
+            // }
+            // echo $math;
+            // .........................................
+            // $studentIs = "Shanto";
+            // if (!is_null($studentIs))
+            // echo "$studentIs Is not here";
+            // empty...............................................
+            // $studentName = "Akas";
+            // echo empty($studentName);
+            // if(!empty($studentName)){
+            //     echo "$studentName Is a good Student";
+            // }
+            // array key edist.................................................
+
+            // $data = [
+            //     'roll' => 26,
+            //     'name' => 'shanto'
+
+            // ];
+            // if (array_key_exists('number', $data)) {
+
+            //     echo "This Is shanto";
+            // }
             require_once "2nd.php";
-            $sum = "$x + $y";
-            echo $sum;
-
+            // echo "<br>";
+            // // $text = (serialize($data));
+            // // var_dump(unserialize($text));
+            // print_r($data);
+            // unset($data['roll']);
+            // echo "<br>";
+            // print_r($data);
             ?>
-        </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-</body>
-
-</html>
+ 
